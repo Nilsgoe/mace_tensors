fixed the memory leak of the basis transformation spherical to cartesian, also the normalization is now fixed but for the test set is still needs to be tested!

mace/calculators/mace.py
@@ -266,6 +266,12 @@ class MACECalculator(Calculator):
         for model in self.models:
             for param in model.parameters():
                 param.requires_grad = False
+        print("the state dict of the model is:")
+        print(self.models[0].state_dict().keys())
+        print(self.models[0].state_dict()["dipole_mean"])
+        print(self.models[0].state_dict()["dipole_std"])
+        print(self.models[0].state_dict()["polarizability_mean"])
+        print(self.models[0].state_dict()["polarizability_std"])    
         try:
             if model.state_dict()["dipole_mean"] is not None:
                 self.dipole_mean = model.state_dict()["dipole_mean"].to(self.device)
@@ -510,10 +516,11 @@ class MACECalculator(Calculator):
         if self.model_type in [
             "DipolePolarizabilityMACE",
         ]:
+            
             self.results["polarizability"] = (
                 torch.mean(ret_tensors["polarizability"], dim=0).cpu().numpy()
-                * self.polarizability_std.cpu().numpy()
-                + self.polarizability_mean.cpu().numpy()
+                #* self.polarizability_std.cpu().numpy()
+                #+ self.polarizability_mean.cpu().numpy()
             )
             self.results["polarizability_sh"] = (
                 torch.mean(ret_tensors["polarizability_sh"], dim=0).cpu().numpy()

mace/cli/run_train.py
@@ -65,6 +65,25 @@ from mace.tools.scripts_utils import (
 from mace.tools.slurm_distributed import DistributedEnvironment
 from mace.tools.tables_utils import create_error_table
 from mace.tools.utils import AtomicNumberTable
+from copy import deepcopy
+import torch.utils as tutils
+
+class NormalizeOnFlyDataset(tutils.data.Dataset):
+    def __init__(self, base_dataset, dipole_mean, dipole_std, polar_mean, polar_std):
+        self.base_dataset = base_dataset
+        self.dipole_mean = dipole_mean
+        self.dipole_std = dipole_std
+        self.polar_mean = polar_mean
+        self.polar_std = polar_std
+
+    def __getitem__(self, idx):
+        data = deepcopy(self.base_dataset[idx])  # copy to avoid modifying original
+        data.dipole = (data.dipole - self.dipole_mean) / self.dipole_std
+        data.polarizability = (data.polarizability - self.polar_mean) / self.polar_std
+        return data
+
+    def __len__(self):
+        return len(self.base_dataset)
 
 
 def main() -> None:
@@ -612,20 +631,29 @@ def run(args) -> None:
             logging.info(f"Mean dipole: {dipoles_mean}, std: {dipoles_std}")
             logging.info(f"Mean polarizability: {polarizabilities_mean}, std: {polarizabilities_std}")
             print("Normalizing dipoles and polarizabilities for head:", head_config.head_name)
-            for train_set in train_sets[head_config.head_name]:
-                #print(train_set["dipole"])
+            '''for train_set in train_sets[head_config.head_name].datasets:
+                print(train_set)
+                exit()
+                #print(head_config.head_name)
                 train_set["dipole"] = (train_set["dipole"] - dipoles_mean) / dipoles_std
-                #print(train_set["dipole"])
+                #print("Before:",train_set["polarizability"])
                 train_set["polarizability"] = (train_set["polarizability"] - polarizabilities_mean) / polarizabilities_std
-                #exit()
+                #print("After:",train_set["polarizability"])'''
+           
+            normalized_datasets = [
+                NormalizeOnFlyDataset(ds, dipoles_mean, dipoles_std, polarizabilities_mean, polarizabilities_std)
+                for ds in train_sets[head_config.head_name].datasets
+            ]
 
+            train_sets[head_config.head_name] = tutils.data.ConcatDataset(normalized_datasets)
 
-            for valid_set in valid_sets[head_config.head_name]:
-                #print(train_set["dipole"])
-                valid_set["dipole"] = (valid_set["dipole"] - dipoles_mean) / dipoles_std
-                #print(train_set["dipole"])
-                valid_set["polarizability"] = (valid_set["polarizability"] - polarizabilities_mean) / polarizabilities_std
-                #exit()
+            normalized_val_datasets = [
+                NormalizeOnFlyDataset(ds, dipoles_mean, dipoles_std, polarizabilities_mean, polarizabilities_std)
+                for ds in valid_sets[head_config.head_name].datasets
+            ]
+
+            valid_sets[head_config.head_name] = tutils.data.ConcatDataset(normalized_val_datasets)
+            
             #scale_shift=(dipoles_mean, dipoles_std, polarizabilities_mean, polarizabilities_std)
         # Create data loader for this head
         if isinstance(train_sets[head_config.head_name], list):
@@ -896,6 +924,7 @@ def run(args) -> None:
                     )
                     for config in subset
                 ]
+        logging.info(f"Time for the test dir {head_config.test_dir}")
         if head_config.test_dir is not None:
             if not args.multi_processed_test:
                 test_files = get_files_with_suffix(head_config.test_dir, "_test.h5")
@@ -904,6 +933,13 @@ def run(args) -> None:
                     test_sets[name] = data.HDF5Dataset(
                         test_file, r_max=args.r_max, z_table=z_table, heads=heads, head=head_config.head_name
                     )
+                    if args.normalize_dipole_polar is True:
+                        logging.info(f"Normalizing the test sets {name}")
+                        for i in range(len(test_sets[name])):
+                            test_example = test_sets[name][i]
+                            test_example["dipole"] = (test_example["dipole"] - dipoles_mean) / dipoles_std
+                            test_example["polarizability"] = (test_example["polarizability"] - polarizabilities_mean) / polarizabilities_std        
+
             else:
                 test_folders = glob(head_config.test_dir + "/*")
                 for folder in test_folders:

mace/modules/models.py
@@ -12,7 +12,7 @@ from e3nn.util.jit import compile_mode
 
 from mace.modules.radial import ZBLBasis
 from mace.tools.scatter import scatter_mean, scatter_sum
-from mace.tools.torch_tools import spherical_to_cartesian
+from mace.tools.torch_tools import spherical_to_cartesian,get_change_of_basis
 
 from .blocks import (
     AtomicEnergiesBlock,
@@ -793,6 +793,7 @@ class AtomicDielectricMACE(torch.nn.Module):
         )  # 3x3 matrix flattened
         self.use_polarizability = use_polarizability
         self.register_buffer("polarizability_std", torch.ones(3, 3))
+        self.register_buffer("change_of_basis", get_change_of_basis())
         # self.register_buffer("mean_polarizability_sh", torch.zeros(6))
         # self.register_buffer("std_polarizability_sh", torch.ones(6))
         if means_stds is not None:
@@ -1043,7 +1044,7 @@ class AtomicDielectricMACE(torch.nn.Module):
                 dim_size=num_graphs,
             )  # [n_graphs,6]
             total_polarizability = spherical_to_cartesian(
-                total_polarizability_spherical
+                total_polarizability_spherical, self.change_of_basis
             )
 
             if compute_dielectric_derivatives: